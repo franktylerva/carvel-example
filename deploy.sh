887b5a6ae4555451362c91fcdea25489104b2fd3
ftyler <ftyler.ctr@kr.af.mil>
2022-04-28 13:59:07 -0400

    Add crud-aggregator to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,5 @@ kapp deploy -a atlas -f <(kubectl create ns kafka --dry-run=client -o yaml) \
     -f <(helm template kafka confluentinc/cp-helm-charts --skip-tests --values ./kafka/values.yaml | ytt -f- -f ./kafka/config/) \
     -f <(helm template infrastructure ./infrastructure --skip-tests | kubectl apply -f- --dry-run=client -o yaml -n atlas-infrastructure) \
     -f <(helm template observation-crud bitnami/postgresql --skip-tests -n atlas-observation-crud-service --values ./postgresql/values.yaml | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service) \
-    -f <(helm template observation-crud ./observation-crud --skip-tests -n atlas-observation-crud-service | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service)
+    -f <(helm template observation-crud ./observation-crud --skip-tests -n atlas-observation-crud-service | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service) \
+    -f <(helm template crud-aggregator ./crud-aggregator --skip-tests -n atlas-observation-crud-service | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service)
