9c8f82e0c8fd862b49498f570b3af0d64d225cc9
ftyler <ftyler.ctr@kr.af.mil>
2022-04-28 09:24:35 -0400

    Added manifests for crud-aggregator and add keycloak service.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 kapp deploy -a atlas -f <(kubectl create ns kafka --dry-run=client -o yaml) \
     -f <(kubectl create ns atlas-infrastructure --dry-run=client -o yaml) \
     -f <(kubectl create ns atlas-observation-crud-service --dry-run=client -o yaml) \
+    -f <(kubectl create ns keycloak --dry-run=client -o yaml) \
+    -f <(helm template keycloak bitnami/keycloak --skip-tests --values ./keycloak/values.yaml -n keycloak | ytt -f- -f ./keycloak/config/) \
     -f <(kubectl create configmap scripts -n atlas-observation-crud-service --from-file=./sql --dry-run=client -o yaml) \
     -f <(helm template kafka confluentinc/cp-helm-charts --skip-tests --values ./kafka/values.yaml | ytt -f- -f ./kafka/config/) \
     -f <(helm template infrastructure ./infrastructure --skip-tests | kubectl apply -f- --dry-run=client -o yaml -n atlas-infrastructure) \
