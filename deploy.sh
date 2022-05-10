828973264dd17a353a6c7ca9d1362263fedad336
ftyler <ftyler.ctr@kr.af.mil>
2022-05-10 14:37:34 -0400

    Updates to crud-aggregator.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ kapp deploy -a atlas -f <(kubectl create ns kafka --dry-run=client -o yaml) \
     -f <(kubectl create ns atlas-infrastructure --dry-run=client -o yaml) \
     -f <(kubectl create ns atlas-observation-crud-service --dry-run=client -o yaml) \
     -f <(kubectl create ns keycloak --dry-run=client -o yaml) \
+    -f <(kubectl create ns atlas-orbital-crud-service --dry-run=client -o yaml) \
+    -f <(kubectl create ns atlas-sensor-crud-service --dry-run=client -o yaml) \
     -f <(helm template keycloak bitnami/keycloak --skip-tests --values ./keycloak/values.yaml -n keycloak | ytt -f- -f ./keycloak/config/) \
     -f <(kubectl create configmap scripts -n atlas-observation-crud-service --from-file=./sql --dry-run=client -o yaml) \
     -f <(helm template kafka confluentinc/cp-helm-charts --skip-tests --values ./kafka/values.yaml | ytt -f- -f ./kafka/config/) \
