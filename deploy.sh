fc41248efb03c51eb6a0ad53e6b416402f7779cf
ftyler <ftyler.ctr@kr.af.mil>
2022-05-11 10:09:18 -0400

    Remove ordering from Helm Charts.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-kapp deploy -a atlas -f <(kubectl create ns kafka --dry-run=client -o yaml) \
-    -f <(kubectl create ns atlas-infrastructure --dry-run=client -o yaml) \
-    -f <(kubectl create ns atlas-observation-crud-service --dry-run=client -o yaml) \
-    -f <(kubectl create ns keycloak --dry-run=client -o yaml) \
-    -f <(kubectl create ns atlas-orbital-crud-service --dry-run=client -o yaml) \
-    -f <(kubectl create ns atlas-sensor-crud-service --dry-run=client -o yaml) \
-    -f <(helm template keycloak bitnami/keycloak --skip-tests --values ./keycloak/values.yaml -n keycloak | ytt -f- -f ./keycloak/config/) \
-    -f <(kubectl create configmap scripts -n atlas-observation-crud-service --from-file=./sql --dry-run=client -o yaml) \
-    -f <(helm template kafka confluentinc/cp-helm-charts --skip-tests --values ./kafka/values.yaml | ytt -f- -f ./kafka/config/) \
-    -f <(helm template infrastructure ./infrastructure --skip-tests | kubectl apply -f- --dry-run=client -o yaml -n atlas-infrastructure) \
-    -f <(helm template observation-crud bitnami/postgresql --skip-tests -n atlas-observation-crud-service --values ./postgresql/values.yaml | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service) \
-    -f <(helm template observation-crud ./observation-crud --skip-tests -n atlas-observation-crud-service | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service) \
-    -f <(helm template crud-aggregator ./crud-aggregator --skip-tests -n atlas-observation-crud-service | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service)
+kapp deploy -a atlas -f config.yaml
+
+# kapp deploy -a atlas -f <(kubectl create ns kafka --dry-run=client -o yaml) \
+#     -f <(kubectl create ns atlas-infrastructure --dry-run=client -o yaml) \
+#     -f <(kubectl create ns atlas-observation-crud-service --dry-run=client -o yaml) \
+#     -f <(kubectl create ns keycloak --dry-run=client -o yaml) \
+#     -f <(kubectl create ns atlas-orbital-crud-service --dry-run=client -o yaml) \
+#     -f <(kubectl create ns atlas-sensor-crud-service --dry-run=client -o yaml) \
+#     -f <(helm template keycloak bitnami/keycloak --skip-tests --values ./keycloak/values.yaml -n keycloak | ytt -f- -f ./keycloak/config/) \
+#     -f <(kubectl create configmap scripts -n atlas-observation-crud-service --from-file=./sql --dry-run=client -o yaml) \
+#     -f <(helm template kafka confluentinc/cp-helm-charts --skip-tests --values ./kafka/values.yaml | ytt -f- -f ./kafka/config/) \
+#     -f <(helm template infrastructure ./infrastructure --skip-tests | kubectl apply -f- --dry-run=client -o yaml -n atlas-infrastructure) \
+#     -f <(helm template observation-crud bitnami/postgresql --skip-tests -n atlas-observation-crud-service --values ./postgresql/values.yaml | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service) \
+#     -f <(helm template observation-crud ./observation-crud --skip-tests -n atlas-observation-crud-service | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service) \
+#     -f <(helm template crud-aggregator ./crud-aggregator --skip-tests -n atlas-observation-crud-service | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service)
---
+++ update-images.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+OUTPUT=config.yaml
+
+kubectl create ns kafka --dry-run=client -o yaml > $OUTPUT
+echo --- >> $OUTPUT
+
+kubectl create ns atlas-infrastructure --dry-run=client -o yaml >> $OUTPUT
+echo --- >> $OUTPUT
+
+kubectl create ns atlas-observation-crud-service --dry-run=client -o yaml >> $OUTPUT
+echo --- >> $OUTPUT
+
+kubectl create ns keycloak --dry-run=client -o yaml >> $OUTPUT
+echo --- >> $OUTPUT
+
+kubectl create ns atlas-orbital-crud-service --dry-run=client -o yaml >> $OUTPUT
+echo --- >> $OUTPUT
+
+kubectl create ns atlas-sensor-crud-service --dry-run=client -o yaml >> $OUTPUT
+echo --- >> $OUTPUT
+
+helm template keycloak bitnami/keycloak --skip-tests --values ./keycloak/values.yaml -n keycloak >> $OUTPUT
+echo --- >> $OUTPUT
+
+kubectl create configmap scripts -n atlas-observation-crud-service --from-file=./sql --dry-run=client -o yaml >> $OUTPUT
+echo --- >> $OUTPUT
+
+helm template kafka confluentinc/cp-helm-charts --skip-tests --values ./kafka/values.yaml >> $OUTPUT
+echo --- >> $OUTPUT
+
+helm template infrastructure ./infrastructure --skip-tests | kubectl apply -f- --dry-run=client -o yaml -n atlas-infrastructure >> $OUTPUT
+echo --- >> $OUTPUT
+
+helm template observation-crud bitnami/postgresql --skip-tests -n atlas-observation-crud-service --values ./postgresql/values.yaml | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service >> $OUTPUT
+echo --- >> $OUTPUT
+
+helm template observation-crud ./observation-crud --skip-tests -n atlas-observation-crud-service | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service >> $OUTPUT
+echo --- >> $OUTPUT
+
+helm template crud-aggregator ./crud-aggregator --skip-tests -n atlas-observation-crud-service | kubectl apply -f- --dry-run=client -o yaml -n atlas-observation-crud-service >> $OUTPUT
+echo --- >> $OUTPUT
